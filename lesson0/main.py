6d981653ed8d69209774d2634e8efe3b8103a1a0
isaacteye <isaacteye@outlook.de>
2024-06-26 16:34:24 +0200

    I deleted some few comments

--- lesson0/main.py
+++ lesson0/main.py
@@ -1,4 +1,10 @@
 # this is a tryout 
 
 print('hello fire')
-
+#pull request
+#git log
+#git status 
+#git add .
+#git commit -m
+#git push or (git pull...git fetch)pull request
+#staging area, commiting, merging 
