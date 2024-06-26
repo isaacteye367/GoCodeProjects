32ed13514ee2cba9f58080198098df3d58e22113
Weten-Ogen <alphaoware@gmail.com>
2024-06-26 14:21:28 +0000

    merge resolved

--- lesson0/main.py
+++ lesson0/main.py
@@ -5,4 +5,7 @@ print('hello fire')
 # git log 
 # git status
 # git add .
+# git commit -m "type inany message of your choice"
+# git push or (git pull... git fetch )pull request
+# staging area ,commiting, merging 
 # 
